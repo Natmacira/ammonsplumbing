7a2ae6788cf74f12f7a2ccb5d635faa99aeb7de7
natmacira <natimciraolo@gmail.com>
2021-08-31 13:11:58 -0300

    last version js

--- js/contact.js
+++ js/contact.js
@@ -2,31 +2,37 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const numbersForCaptcha = document.getElementById('captcha-random');
 
-    const resultAddition    = document.getElementById('result-value');
+    const resultAddition = document.getElementById('result-value');
 
-    const secretResult      = document.getElementById('secret-result');
+    const secretResult = document.getElementById('secret-result');
 
-    const contactForm       = document.getElementById('contact-form');
+    const contactForm = document.getElementById('contact-form');
 
-    const wrongNumber       = document.querySelector('.paragraph-wrong-number');
+    const wrongNumber = document.querySelector('.paragraph-wrong-number');
+
+    const goodNumber = document.querySelector('.paragraph-good-number')
 
     function generateCaptcha() {
         let randomNumber = Math.floor(Math.random() * 10);
         let randomNumberTwo = Math.floor(Math.random() * 10);
         numbersForCaptcha.innerText = randomNumber + ' + ' + randomNumberTwo + ' = ';
 
-        secretResult.value = randomNumber + randomNumberTwo; 
+        secretResult.value = randomNumber + randomNumberTwo;
     }
     generateCaptcha();
 
-    contactForm.addEventListener('submit', function (e){
+    contactForm.addEventListener('submit', function (e) {
         e.preventDefault();
-        
-        if ( secretResult.value === resultAddition.value ) {
-            this.submit();   
-        } else { 
-        
+
+        if (secretResult.value === resultAddition.value) {
+
+            goodNumber.classList.add('good-number-in-captcha');
+            wrongNumber.classList.remove('wrong-number-in-captcha');
+
+        } else {
+
             wrongNumber.classList.add('wrong-number-in-captcha');
+            goodNumber.classList.remove('good-number-in-captcha');
 
         }
 
