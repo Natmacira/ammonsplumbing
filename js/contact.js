faeb50db43739ba16e48b74efb74e560519d73f8
natmacira <natimciraolo@gmail.com>
2021-09-02 14:50:33 -0300

    responsive 768 index1

--- js/contact.js
+++ js/contact.js
@@ -38,7 +38,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
 
+    const burgerMenuBtn = document.querySelector("#burger-menu-toggler")
 
+    const menuItems = document.querySelectorAll(".menu-item")
+
+    function navResponsive() {
+
+        burgerMenuBtn.addEventListener('click', function () {
+            document.body.classList.toggle('mobile-menu-active');
+        });
+
+        menuItems.forEach(function (menuItem) {
+            menuItem.addEventListener('click', function () {
+                document.body.classList.remove('mobile-menu-active');
+
+                let currentItem = document.querySelector('.active');
+                currentItem.classList.remove('active');
+                this.classList.add('active');
+
+            })
+        })
+
+    }
+
+    navResponsive();
 
 
 
